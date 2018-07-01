4d51b106eb928b1246ccafd167996c036a3ad028
Ray Li <ray.li.verified@gmail.com>
2018-07-01 14:48:41 -0500

    Update AndroidX
    *Update to AndroidX.
    *Update Gradle to v3.2.0-beta02.
    *TargetSDK v28.

--- app/src/main/java/com/unstoppable/submitbutton/MainActivity.java
+++ app/src/main/java/com/unstoppable/submitbutton/MainActivity.java
@@ -2,7 +2,7 @@ package com.unstoppable.submitbutton;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
